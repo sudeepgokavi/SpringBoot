dab8322936e22c3e47d3fb1539cf288f4015c3f3
Sudeep G <sudeepgokavi@yahoo.com>
2017-03-14 18:22:38 +0530

    Changed

--- SpringBoot/src/com/springboot/App.java
+++ SpringBoot/src/com/springboot/App.java
@@ -3,10 +3,7 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
+
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
